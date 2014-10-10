66f9280c65f3c62d54f85c1b10087109cf6b02b3
Beanow <beanow@project-virality.com>
2014-10-10 18:33:53 +0200

    Remove implementation specific methods from the interface.

--- src/Responder/ResponderInterface.php
+++ src/Responder/ResponderInterface.php
@@ -3,13 +3,29 @@
 use Aura\Web\Response;
 use Tuxion\DoctrineRest\Domain\Result\ResultInterface;
 
+/**
+ * An interface to make sure that responders always accept results and can be invoked.
+ */
 interface ResponderInterface
 {
   
+  /**
+   * Returns the ResultInterface that holds the final state of the application to communicate to the user.
+   * @return ResultInterface
+   */
   public function getResult();
-  public function getResponse();
+  
+  /**
+   * Sets the ResultInterface that holds the final state of the application to communicate to the user.
+   * @param ResultInterface $result
+   * @return void
+   */
   public function setResult(ResultInterface $result);
-  public function __construct(Response $response, StatusCodes $statusCodes);
+  
+  /**
+   * Sets the response on an Aura Response object and returns it.
+   * @return Response
+   */
   public function __invoke();
   
 }
